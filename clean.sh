91a7f5d60ef95ed054c38894a713a5d97e8de5eb
Marcin Osowski <marcin.osowski@students.mimuw.edu.pl>
2012-10-27 11:53:41 +0200

    Made Latte.cf grammar compilable.

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+make distclean
+#rm -f Hasky
--- recompile.sh
+++ recompile.sh
@@ -6,6 +6,13 @@ set -e
 
 bnfc -cpp_stl -l -p bnfc -m Latte.cf
 
+# Add <algorithm> header to Bison input file
+sed -i Latte.y -e 's/#include <iostream>/#include <iostream>\n#include <algorithm>/'
+
+# Add annotation that we expect one shift/reduce conflict (dangling else problem)
+sed -i Latte.y -e 's/^%%$/%expect 1\n\n%%/'
+
+
 make clean
 make
 
